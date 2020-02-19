2ca626052d7bd6318989a3815f3dbd5f9dfa6f5f
Baptiste Dulac <dulacbaptiste@gmail.com>
2020-02-20 03:20:07 +0800

    Add ST_GeomFromText function and change ST_Distance_Sphere max number of parameters

--- lib/CrEOF/Spatial/ORM/Query/AST/Functions/MySql/STGeomFromText.php
+++ lib/CrEOF/Spatial/ORM/Query/AST/Functions/MySql/STGeomFromText.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Copyright (C) 2013 luca capra
+ * Copyright (C) 2012 Derek J. Lambert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,16 +26,18 @@ namespace CrEOF\Spatial\ORM\Query\AST\Functions\MySql;
 use CrEOF\Spatial\ORM\Query\AST\Functions\AbstractSpatialDQLFunction;
 
 /**
- * STDistance DQL function
+ * GeomFromText DQL function
  *
- * @author  luca capra <luca.capra@create-net.org>
+ * @author  Derek J. Lambert <dlambert@dereklambert.com>
  * @license http://dlambert.mit-license.org MIT
  */
-class STDistanceSphere extends AbstractSpatialDQLFunction {
-
+class STGeomFromText extends AbstractSpatialDQLFunction
+{
     protected $platforms = array('mysql');
-    protected $functionName = 'ST_Distance_Sphere';
-    protected $minGeomExpr = 2;
-    protected $maxGeomExpr = 3;
 
+    protected $functionName = 'ST_GeomFromText';
+
+    protected $minGeomExpr = 1;
+
+    protected $maxGeomExpr = 1;
 }
